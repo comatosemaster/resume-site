d054500f1c0e29c03a4867b8bd71207bcdbd2fb7
Davit Mujirishvili <mujirishvilidato6@gmail.com>
2025-12-07 22:04:07 +0400

    Move SMTP secrets to env variables

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,8 +105,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "heyitsdavit@gmail.com"          # your Gmail
-EMAIL_HOST_PASSWORD = "dkjt ljvn hwpg wcxx"     # NOT your normal password!
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")          # your Gmail
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")     # NOT your normal password!
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
